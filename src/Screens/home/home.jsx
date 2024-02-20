322a100a2b0263cd0e1b961d8d8ba3f8821c9498
Krishna311203 <krishnashrivastava84@gmail.com>
2024-02-20 20:09:30 +0530

    added spotify APi

--- src/Screens/home/home.jsx
+++ src/Screens/home/home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import Library from '../library/library';
 import Feed from '../feed/feed';
@@ -7,25 +7,44 @@ import Favorites from '../favorites/favorites';
 import Trending from '../trending/trending';
 import './home.css';
 import SideBar from '../../Components/SideBar/SideBar';
+import Login from '../Auth/login';
+import { useEffect } from 'react';
+import { setClientToken } from '../../spotify';
 
 
 const Home = () => {
-  return (
-<div className="main-body">
-<Router>
-<SideBar/>
-      <Routes>
-        <Route path="/" element={<Library/>} />
-        <Route path="/feed" element={<Feed/>} />
-        <Route path="/player" element={<Player/>} />
-        <Route path="/library" element={<Library/>} />
-        <Route path="/trending" element={<Trending/>} />
-        <Route path="/favorites" element={<Favorites/>} />
-      </Routes>
-    </Router>
-</div>
-    
-  );
-};
 
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    const hash = window.location.hash;
+    window.location.hash = "";
+    if (!token && hash) {
+      const _token = hash.split("&")[0].split("=")[1];
+      window.localStorage.setItem("token", _token);
+      setToken(_token);
+      setClientToken(_token);
+    } else {
+      setToken(token);
+      setClientToken(token);
+    }
+  }, []);
+  return !token ? (
+    <Login />
+  ) : (
+    <Router>
+      <div className="main-body">
+       <SideBar />
+        <Routes>
+          <Route path="/" element={<Library />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/trending" element={<Trending />} />
+          <Route path="/player" element={<Player />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 export default Home;
